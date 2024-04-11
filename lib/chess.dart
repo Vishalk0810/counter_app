d441739f86dc348616d2a4caf49725e7ab13b2e4
Vishal Keshri <vishalkeshri858@gmail.com>
2024-04-11 18:49:47 +0530

    11-04-2024

---
+++ lib/chess.dart
@@ -0,0 +1,188 @@
+import 'package:counter_app/counter_app.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: Icon(Icons.menu,color: Colors.white,),
+        centerTitle: true,
+        title: Text(
+          'Chess',style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            border: Border.all(width: 0.32,color: Colors.black)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            row1(),
+            row2(),
+            row1(),
+            row2(),
+            row1(),
+            row2(),
+            row1(),
+            row2(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row row2() {
+    return Row(
+      children: [
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row row1() {
+    return Row(
+      children: [
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+        ),
+        Container(
+          height: 49,
+          width: 49,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade400,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,213 +1,188 @@
-import 'package:flutter/cupertino.dart';
+import 'package:counter_app/counter_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Myapp());
+void main()
+{
+  runApp(MyApp());
 }
-
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Calcscreen(),
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
 
-class Calcscreen extends StatefulWidget {
-  const Calcscreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<Calcscreen> createState() => _HomescreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomescreenState extends State<Calcscreen> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: Icon(Icons.menu,color: Colors.white,),
         centerTitle: true,
-        backgroundColor: Color(0xff54759E),
-        leading: Icon(
-          Icons.menu,
+        title: Text(
+          'Chess',style: TextStyle(
           color: Colors.white,
+          fontWeight: FontWeight.bold,
         ),
-        title: const Text(
-          'Calc',
-          style: TextStyle(
-            color: Colors.white,
-
-            fontWeight: FontWeight.bold,
-
-            // backgroundColor:
-          ),
         ),
-
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            '$count',
-            style: const TextStyle(
-              color: Color(0xff9E9E9E),
-              fontSize: 75,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Row(
-
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              // mainAxisAlignment: MainAxisAlignment.,
-              children: [
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      count -= 2;
-                    });
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Color(0xff54759E),
-                      borderRadius: BorderRadiusDirectional.circular(10),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '-2',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      count += 2;
-                    });
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Color(0xff54759E),
-                      borderRadius: BorderRadiusDirectional.circular(10),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '+2',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      count -= 4;
-                    });
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Color(0xff54759E),
-                      borderRadius: BorderRadiusDirectional.circular(10),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '-4',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      count += 4;
-                    });
-                  },
-                  child: Container(
-                    height: 50,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Color(0xff54759E),
-                      borderRadius: BorderRadiusDirectional.circular(10),
-                    ),
-                    child:const Center(
-                      child: Text(
-                        '+4',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: InkWell(
-              onTap: () {
-                setState(() {
-                  count = 0;
-                });
-              },
-              child: Container(
-                height: 50,
-                width: 150,
-                decoration: BoxDecoration(
-                  color: Color(0xff54759E),
-                  borderRadius: BorderRadiusDirectional.circular(10),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Clear',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 25,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 0.32,color: Colors.black)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            row1(),
+            row2(),
+            row1(),
+            row2(),
+            row1(),
+            row2(),
+            row1(),
+            row2(),
+          ],
+        ),
       ),
     );
   }
-}
 
-int count = 0;
+  Row row2() {
+    return Row(
+          children: [
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+          ],
+        );
+  }
+
+  Row row1() {
+    return Row(
+          children: [
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            Container(
+              height: 49,
+              width: 49,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade400,
+              ),
+            ),
+          ],
+        );
+  }
+}
